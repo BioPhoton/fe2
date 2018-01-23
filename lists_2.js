81d4308c87b20744208fd78395187ac79b3196ed
Michael Hladky <michael@hladky.at>
2018-01-23 22:32:12 +0100

    Create lists_2.js

---
+++ lists_2.js
@@ -0,0 +1,29 @@
+const data = [
+{id: 1},
+{id: 2},
+{id: 3},
+{id: 4},
+{id: 5}
+];
+
+// 2. Find the item with id:5
+
+// 2.1. Create a function to convert the array to an id map
+const toIdMap = (array) => {
+  //
+}
+
+// 2.2. Create a function to convert the idMap to an array
+const toArray = (idMap) => {
+  //
+}
+
+// 2.3. Find the element in the mapped object
+const getId5 = () => {
+  //
+}
+
+// 2.4. Create a function that returns all items as an array
+const getAll() {
+  //
+}
