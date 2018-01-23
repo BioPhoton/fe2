e71e75a77c0c3eba82fefdca970962de9058cfd5
Michael Hladky <michael@hladky.at>
2018-01-23 22:27:42 +0100

    Create lists_1.js

---
+++ lists_1.js
@@ -0,0 +1,24 @@
+const data = [
+{id: 1},
+{id: 2},
+{id: 3},
+{id: 4},
+{id: 5}
+];
+
+// 1. Find the item with id:5
+//    Use forEach
+const getId5 = () => {
+  //
+}
+
+// 2. Find the item with id:5
+//    Create a function to convert the array to an id map
+//    Find the element in the mapped object
+const toIdMap = (array) => {
+  //
+}
+
+const getId5 = () => {
+  //
+}
